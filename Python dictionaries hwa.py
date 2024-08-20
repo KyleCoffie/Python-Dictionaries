cfa8a54cf9e3feb2f10e0aaba45c9f0ee60860fa
KyleCoffie <160148467+KyleCoffie@users.noreply.github.com>
2024-08-19 21:23:48 -0500

    second commit

--- Python dictionaries hwa.py
+++ Python dictionaries hwa.py
@@ -15,18 +15,13 @@ restaurant_menu = {
     "Main Course": {"Steak": 15.99, "Salmon": 13.99},
     "Desserts": {"Cake": 4.99, "Ice Cream": 3.99}}
 #- Add a new category called "Beverages" with at least two items.
-restaurant_menu["Beverages"]= "Water", "Iced Tea"
-
-for course, item, in restaurant_menu.items():
-    print(item)
-for key in restaurant_menu.keys():
-    print(key)
-    
+restaurant_menu["Beverages"]= {"Water": 1.00, "Iced Tea": 1.10}
+print(restaurant_menu)
 
 #- Update the price of "Steak" to 17.99.
 restaurant_menu["Main Course"]["Steak"] = "17.99"
 print (restaurant_menu)
 
 #- Remove "Bruschetta" from "Starters". 
-removed_item = course,item.popitem("Bruschetta")
-print(removed_item)
+del restaurant_menu["Starters"]["Bruschetta"]
+print(restaurant_menu)
